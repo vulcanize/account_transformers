161fa9664ea5eee30acdb82072e32eafbdb293d2
anon <sarah.anne.friend@gmail.com>
2019-04-23 18:57:19 -0400

    Add indexes, remove old view

--- db/migrations/20190417082953_create_indexes_token_value_transfers.sql
+++ db/migrations/20190417082953_create_indexes_token_value_transfers.sql
@@ -1,13 +1,13 @@
 -- +goose Up
 -- +goose StatementBegin
-CREATE INDEX CONCURRENTLY address_src
+CREATE INDEX address_src
 ON accounts.token_value_transfers(src, block_number);
-CREATE INDEX CONCURRENTLY address_dest
-ON accounts.token_value_transfers(dest, block_number);
+CREATE INDEX address_dst
+ON accounts.token_value_transfers(dst, block_number);
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-DROP INDEX address_dest;
+DROP INDEX address_dst;
 DROP INDEX address_src;
 -- +goose StatementEnd
